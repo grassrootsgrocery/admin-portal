b8c09402d12a4ac3aa97b9c38b789de9ab4f4326
Aryan Das <adas1@umd.edu>
2022-03-24 09:55:27 -0400

    started prev events things

--- my-app/src/Components/Home/PreviousEvents.js
+++ my-app/src/Components/Home/PreviousEvents.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import "./Prev.css";
 
 function PreviousEvents() {
+  function addEvent(){
+
+  }
   return (
-    <div>Previous Events</div>
+    <div className="container"> 
+      <div className="title">
+        <h1>Previous Events</h1>
+      </div>
+      <div className="event_one">
+        <div className="left_col">
+          <div className="date">
+            <p>March 10th, 2022</p>
+          </div>
+          <div className="group">
+            <p>Volunteer Group</p>
+            <h3>Hack4Impact</h3>
+          </div>
+          <div className="pickup">
+            <p>Pickup Location</p>
+            <h3>Lighthouse</h3>
+          </div>
+        </div>
+        <div className="right_col">
+          <div className="turnout">
+            <p>Participant Turnout</p>
+          </div>
+          <div className="group_drivers">
+            <p>8 Hack4Impact Drivers</p>
+          </div>
+          <div className="general_drivers">
+            <p>17 General Drivers</p>
+          </div>
+        </div>
+      </div>
+      <div className="event_two">
+        <h1>Event Two Box</h1>
+      
+      </div>
+      <button className="see_more">See More</button>
+    </div>
   )
 }
 

664371223cfbf1f7343c1d24c3bc4bb108ddde95
Sadena Rishindran <sadena.rishindran@gmail.com>
2022-03-23 22:04:47 -0400

    Initial navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
+      <BrowserRouter>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <div className="app">
+        <Navbar/>
+        <Home/>
+        <Routes>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/event-coordinator" component={Home}/>
+          <Route exact path="/events" component={Home}/>
+          <Route exact path="/volunteers" component={Home}/>
+          <Route exact path="/cleaning" component={Home}/>
+          <Route exact path="/contact" component={Home}/>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }

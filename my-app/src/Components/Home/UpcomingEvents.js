a7ce2525fe8ccdf5e8d61a583fe34cf485cb04fe
NiharMajmudar <niharmajmudar@comcast.net>
2022-03-30 23:24:10 -0400

    Fix styling in upcoming events panel

--- my-app/src/Components/Home/UpcomingEvents.js
+++ my-app/src/Components/Home/UpcomingEvents.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../../upcomingEvents.css';
+import { Link } from 'react-router-dom';
 
 // Upcoming event has component like day, participants, drivers, volunteers, location, volunteer group, people needed
 // Should make a stateful list of upcoming events that shows maybe the first 2-3 elements on the list
@@ -13,10 +14,14 @@ function Event(props) {
       <div class = "primaryInfoSection">  <b>Volunteer Group</b> <br/> {props.group}</div>
       <div class = "primaryInfoSection"> <b>Pickup Location</b> <br/> {props.location}</div>
       <div class = "primaryInfoSection"> <b>Drivers Needed</b> <br/> {props.needed}</div>
-      <div >Participants:</div>
-      <div class="participantInfo">Drivers: {props.drivers} </div>
-      <div class="participantInfo">Volunteers: {props.volunteers}</div>
+      <div>
+        <br/>
+        Participants:  &nbsp;
+        <span class="participantInfo">Drivers: <span class="participantAmount">{props.drivers} </span></span> &nbsp;
+        <span class="participantInfo">Volunteers: <span class="participantAmount">{props.volunteers} </span></span>
+      </div>
   </div>
+  <br/>
   </>
   )
 }
@@ -28,9 +33,10 @@ function UpcomingEvents() {
     <>
     <div id="upcomingEvents">
       <h1>Upcoming Events</h1>
-      {/* <Event date="Wednesday, March 23" group="Hack4Impact" location="McDonald's" needed={30} drivers={10} volunteers={5} /> */}
+
       {events.map(event => <Event date={event.date} group={event.group} location={event.location} 
       needed={event.needed} drivers={event.drivers} volunteers={event.volunteers} />)}
+      <Link to="/">See more</Link>
     </div>
     </>
   )

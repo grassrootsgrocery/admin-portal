73402dd921898ca607f4ca78824ad6854b93fcbe
Sadena Rishindran <sadena.rishindran@gmail.com>
2022-04-17 15:41:52 -0400

    Fixed most of styling of current events page

--- my-app/src/Components/Home/CurrentEvent.js
+++ my-app/src/Components/Home/CurrentEvent.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import './CurrentEvent.css'
 
 function CurrentEvent(props){
@@ -12,14 +13,7 @@ function CurrentEvent(props){
 
   return (
     <div>
-      {isActive
-      ? <div className="activeText">
-          <div>This event is currently active</div>
-        </div>
-      : <div className="activeText">
-          <div>No active events</div>
-        </div>
-      }
+      <Active isActive={isActive}/>
       <div className='eventBox'>
         <div className='currentEventDate'>{date}</div>
         <div className='eventAttributes'>
@@ -38,61 +32,40 @@ function CurrentEvent(props){
             </div>
           </div>
           <div className='eventAttributesRight'>
-            Hi
+            <div className="countContainer">
+              <p className="countText">Drivers</p>
+              <p className="countValue">{driverCount}</p>
+            </div>
+            <div className="countContainer">
+              <p className="countText">Volunteers</p>
+              <p className="countValue">{volunteerCount}</p>
+            </div>
           </div>
+        </div>
+        <div className="currentEventOptions">
+          <Link to="/" className="currentEventOption">Manage This Event</Link>
+          <Link to="/" className="currentEventOption">Visit Webpage</Link>
         </div>
       </div>
     </div>
   );
-
-  // return( 
-  //   <div style={{padding: 10}}>
-  //     <Active />
-  //     <div className='Mcontainer'>
-  //         <div className='date'>{date}</div>
-  //         <EventInfo org = {volGroup} loc = {pickupLoc} drivers_needed = {numDriversNeeded}/>
-  //     <div className = 'container'>
-  //       <h5>Participants: </h5>
-  //       <Counter name= 'Drivers' num={driverCount}/>
-  //       <Counter name= 'Volunteers' num={volunteerCount}/>
-  //     </div>
-  //     <VolunteerTable/>
-  //     <div className='container'>
-  //     <Link to="/" className="currentEventOptions">Manage This Event</Link>
-  //     <Link className="currentEventOptions" name = 'Manage This Event'>Manage This Event</Link>
-  //     <Link className="currentEventOptions" name = 'Recruit'/>
-  //     <Link className="currentEventOptions" name = 'Visit Webpage'/>
-  //     </div>
-  //     </div>
-  //   </div>
-  // );
 }
 
-function EventInfo(props){
-  const org = props.org;
-  const loc = props.loc;
-  const drivers_needed = props.drivers_needed
-  return(
-    <div className='grid'>
-        <text style={{gridColumn: 1, gridRow: 1}}>Volunteer Group</text>
-        <text style={{gridColumn: 2, gridRow: 1}}>Pickup Location</text>
-        <text style={{gridColumn: 3, gridRow: 1}}>Drivers Needed</text>
-        <text style={{gridColumn: 1, gridRow: 2, fontSize: 20, fontWeight: 'bold'}}>{org}</text>
-        <text style={{gridColumn: 2, gridRow: 2, fontSize: 20, fontWeight: 'bold'}}>{loc}</text>
-        <text style={{gridColumn: 3, gridRow: 2, fontSize: 20, fontWeight: 'bold'}}>{drivers_needed}</text>
-    </div>
+function Active(props){
+  const isActive = props.isActive
+  
+  return (
+    <>
+      {isActive
+      ? <div className="activeText">
+          <div>This event is currently active</div>
+        </div>
+      : <div className="activeText">
+          <div>No active events</div>
+        </div>
+      }
+    </>
   )
-}
-
-function VolunteerTable(props){
-  return(
-    <div className='table'>I am a table</div>
-  )
-}
-
-function Link(props) {
-  const name = props.name;
-  return(<text style={{fontSize: 12, paddingTop: 15}}>{name}</text>)
 }
 
 export default CurrentEvent;

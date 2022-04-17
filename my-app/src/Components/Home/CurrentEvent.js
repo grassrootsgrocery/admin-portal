a9938c5d45fcdc7c0934d0423f75761088b420a7
Sadena Rishindran <sadena.rishindran@gmail.com>
2022-04-17 15:15:12 -0400

    Started changing current event

--- my-app/src/Components/Home/CurrentEvent.js
+++ my-app/src/Components/Home/CurrentEvent.js
@@ -1,53 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './CurrentEvent.css'
 
 function CurrentEvent(props){
-  const vol_group = 'Hack4Impact';
-  const pickup_loc = 'Lighthouse';
-  const num_drivers_needed = 25;
-  const date = 'March 14th, 2022';
-  const drivers_have = 16;
-  const volunteers_have = 14;
-  return( 
-    <div style={{padding: 10}}>
-      <Active />
-      <div className='Mcontainer'>
-          <div className='date'>{date}</div>
-          <EventInfo org = {vol_group} loc = {pickup_loc} drivers_needed = {num_drivers_needed}/>
-      <div className = 'container'>
-        <h5>Participants: </h5>
-        <Counter name= 'Drivers' num={drivers_have}/>
-        <Counter name= 'Volunteers' num={volunteers_have}/>
-      </div>
-      <VolunteerTable/>
-      <div className='container'>
-      <Link name = 'Manage'/>
-      <Link name = 'Recruit'/>
-      <Link name = 'Visit Webpage'/>
-      </div>
+  const [volunteerGroup, setVolunteerGroup] = useState('Hack4Impact');
+  const [pickupLoc, setPickUpLoc] = useState('Start Lighthouse');
+  const [driversNeeded, setDriversNeeded] = useState(25);
+  const [date, setDate] = useState('Monday, March 14th');
+  const [driverCount, setDriverCount] = useState(16);
+  const [volunteerCount, setVolunteerCount] = useState(14);
+  const [isActive, setIsActive] = useState(true);
+
+  return (
+    <div>
+      {isActive
+      ? <div className="activeText">
+          <div>This event is currently active</div>
+        </div>
+      : <div className="activeText">
+          <div>No active events</div>
+        </div>
+      }
+      <div className='eventBox'>
+        <div className='currentEventDate'>{date}</div>
+        <div className='eventAttributes'>
+          <div className='eventAttributesLeft'>
+            <div>
+              <p className="attributeTitle">Volunteer Group</p>
+              <p className="attributeValue">{volunteerGroup}</p>
+            </div>
+            <div>
+              <p className="attributeTitle">Pickup Location</p>
+              <p className="attributeValue">{pickupLoc}</p>
+            </div>
+            <div>
+              <p className="attributeTitle">Drivers Needed</p>
+              <p className="attributeValue">{driversNeeded}</p>
+            </div>
+          </div>
+          <div className='eventAttributesRight'>
+            Hi
+          </div>
+        </div>
       </div>
     </div>
   );
-}
 
-//event will need to have an active attribute which this will use
-class Active extends React.Component{
-  render(){
-    let active = true;
-    if(!active){
-      return(<div className='rowC'>
-      <div className='Rcircle'></div>
-      <div className='activeText'>No active events</div>
-      </div>
-      )
-    } else {
-      return(<div className='rowC'>
-      <div className='Gcircle'></div>
-      <div className='activeText'>An event is currently active</div>
-      </div>)
-    }
-
-  }
+  // return( 
+  //   <div style={{padding: 10}}>
+  //     <Active />
+  //     <div className='Mcontainer'>
+  //         <div className='date'>{date}</div>
+  //         <EventInfo org = {volGroup} loc = {pickupLoc} drivers_needed = {numDriversNeeded}/>
+  //     <div className = 'container'>
+  //       <h5>Participants: </h5>
+  //       <Counter name= 'Drivers' num={driverCount}/>
+  //       <Counter name= 'Volunteers' num={volunteerCount}/>
+  //     </div>
+  //     <VolunteerTable/>
+  //     <div className='container'>
+  //     <Link to="/" className="currentEventOptions">Manage This Event</Link>
+  //     <Link className="currentEventOptions" name = 'Manage This Event'>Manage This Event</Link>
+  //     <Link className="currentEventOptions" name = 'Recruit'/>
+  //     <Link className="currentEventOptions" name = 'Visit Webpage'/>
+  //     </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 function EventInfo(props){
@@ -62,17 +80,6 @@ function EventInfo(props){
         <text style={{gridColumn: 1, gridRow: 2, fontSize: 20, fontWeight: 'bold'}}>{org}</text>
         <text style={{gridColumn: 2, gridRow: 2, fontSize: 20, fontWeight: 'bold'}}>{loc}</text>
         <text style={{gridColumn: 3, gridRow: 2, fontSize: 20, fontWeight: 'bold'}}>{drivers_needed}</text>
-    </div>
-  )
-}
-
-function Counter(props){
-  const num = props.num;
-  const name = props.name;
-  return(
-    <div className='drivers'>
-      <h4>{name}</h4>
-      <div className='driversNum'>{num}</div>
     </div>
   )
 }

31d3b80e2bf45c8179e03086ddd5b1eea1e5c6d3
Sadena Rishindran <sadena.rishindran@gmail.com>
2022-05-04 22:38:10 -0400

    Improved to figma design on events page

--- my-app/src/Components/Events/EventButton.js
+++ my-app/src/Components/Events/EventButton.js
@@ -6,7 +6,7 @@ function EventButton() {
     <div className="event-container">
       <div className="event-title">
         <button className="print-button" /*onClick={}*/>
-          <h2 className="print-text">Print Event</h2>
+          <h2 className="print-text">Print Participant Roster</h2>
         </button>
       </div>
     </div>

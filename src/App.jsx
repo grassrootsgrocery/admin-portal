0f923b92822a9ba62751cd303ce7c4ef73f32424
Jason Cavanaugh <jascav418@gmail.com>
2022-10-11 17:56:28 -0400

    Example of how to access airtable API key

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const apiKey = import.meta.env.VITE_AIRTABLE_API_KEY;
+  console.log(apiKey);
 
   return (
     <div className="App"></div>

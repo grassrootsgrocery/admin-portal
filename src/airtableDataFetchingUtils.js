3bfb2a35dd2a30307fda75bee02bf100f2c2b916
Jason Cavanaugh <jascav418@gmail.com>
2022-10-24 08:39:55 -0400

    Added airtableDataFetchingUtils.js

---
+++ src/airtableDataFetchingUtils.js
@@ -0,0 +1,8 @@
+const AIRTABLE_BASE_ID = "app7zige4DRGqIaL2";
+/*
+"Base" is Airtable lingo for database. 
+So this "base id" is the id for the Mott Haven Fridge/Grassroots Groceries Airtable database 
+*/
+
+//"Base" here just has the normal definition
+export const AIRTABLE_URL_BASE = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}`;
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,26 +1,23 @@
 import React, { useState, useEffect } from "react";
-
+import { AIRTABLE_URL_BASE } from "../airtableDataFetchingUtils";
 
 export function Events() {
-  
   const AIRTABLE_API_KEY = import.meta.env.VITE_AIRTABLE_API_KEY;
   const [upcomingEventsData, setUpcomingEventsData] = useState([]);
-  
+
   const fetchUpcomingEventsData = () => {
     fetch(
-      `https://api.airtable.com/v0/app7zige4DRGqIaL2/%F0%9F%9A%9B%20Supplier%20Pickup%20Events?` + 
-      
-      // Get events after today
-      `&filterByFormula=IS_AFTER({Start Time}, NOW())` +
+      `${AIRTABLE_URL_BASE}/%F0%9F%9A%9B%20Supplier%20Pickup%20Events?` +
+        // Get events after today
+        `&filterByFormula=IS_AFTER({Start Time}, NOW())` +
+        // Get fields for upcoming events dashboard
+        `&fields=Start Time` + // Day, Time
+        `&fields=Pickup Address` + // Main Location
+        `&fields=Only Distributor Count` + // Packers, Total Participants
+        `&fields=Only Driver Count` + // Drivers, Total Participants
+        `&fields=Driver and Distributor Count`, // Packers, Drivers, Total Participants
 
-      // Get fields for upcoming events dashboard
-      `&fields=Start Time` +                  // Day, Time
-      `&fields=Pickup Address` +              // Main Location
-      `&fields=Only Distributor Count` +      // Packers, Total Participants
-      `&fields=Only Driver Count` +           // Drivers, Total Participants
-      `&fields=Driver and Distributor Count`, // Packers, Drivers, Total Participants
-
-      {headers: {Authorization: `Bearer ${AIRTABLE_API_KEY}`}}
+      { headers: { Authorization: `Bearer ${AIRTABLE_API_KEY}` } }
     )
       .then((resp) => resp.json())
       .then((data) => {
@@ -37,34 +34,51 @@ export function Events() {
     fetchUpcomingEventsData();
   }, [AIRTABLE_API_KEY]);
 
-  const optionsDay = { weekday: 'long', month: 'long', day: 'numeric' };
-  const optionsTime = { hour: 'numeric', minute: 'numeric', hour12: true };
+  const optionsDay = { weekday: "long", month: "long", day: "numeric" };
+  const optionsTime = { hour: "numeric", minute: "numeric", hour12: true };
 
-  const getOrdinal = function(d) {
-    if (d > 3 && d < 21) return 'th';
+  const getOrdinal = function (d) {
+    if (d > 3 && d < 21) return "th";
     switch (d % 10) {
-      case 1:  return "st";
-      case 2:  return "nd";
-      case 3:  return "rd";
-      default: return "th";
+      case 1:
+        return "st";
+      case 2:
+        return "nd";
+      case 3:
+        return "rd";
+      default:
+        return "th";
     }
-  }
-  
-  const upcomingEvents = upcomingEventsData.map(event => (
-    {
-      day: new Date(event.fields["Start Time"]).toLocaleDateString('en-US', optionsDay) + getOrdinal(new Date(event.fields["Start Time"]).getDate()),      // start day in Weekday, Month Day format
-      time: new Date(event.fields["Start Time"]).toLocaleString('en-US', optionsTime),     // start time in HH:MM AM/PM format
-      mainLocation: event.fields["Pickup Address"][0],
-      numDrivers: event.fields["Only Distributor Count"] + event.fields["Driver and Distributor Count"] + "/30",    // sum of only packers and packers who are also drivers
-      numPackers: event.fields["Only Driver Count"] + event.fields["Driver and Distributor Count"],         // sum of only drivers and drivers who are also packers
-      numtotalParticipants: event.fields["Only Distributor Count"] + event.fields["Only Driver Count"] + event.fields["Driver and Distributor Count"]
-    }
-  ));
+  };
+
+  const upcomingEvents = upcomingEventsData.map((event) => ({
+    day:
+      new Date(event.fields["Start Time"]).toLocaleDateString(
+        "en-US",
+        optionsDay
+      ) + getOrdinal(new Date(event.fields["Start Time"]).getDate()), // start day in Weekday, Month Day format
+    time: new Date(event.fields["Start Time"]).toLocaleString(
+      "en-US",
+      optionsTime
+    ), // start time in HH:MM AM/PM format
+    mainLocation: event.fields["Pickup Address"][0],
+    numDrivers:
+      event.fields["Only Distributor Count"] +
+      event.fields["Driver and Distributor Count"] +
+      "/30", // sum of only packers and packers who are also drivers
+    numPackers:
+      event.fields["Only Driver Count"] +
+      event.fields["Driver and Distributor Count"], // sum of only drivers and drivers who are also packers
+    numtotalParticipants:
+      event.fields["Only Distributor Count"] +
+      event.fields["Only Driver Count"] +
+      event.fields["Driver and Distributor Count"],
+  }));
 
   return (
     <div>
       {upcomingEventsData.length > 0 ? (
-        upcomingEvents.map(event => (
+        upcomingEvents.map((event) => (
           <div>
             <p>Day: {event.day}</p>
             <p>Time: {event.time}</p>
@@ -72,7 +86,7 @@ export function Events() {
             <p>Total Participants: {event.numtotalParticipants}</p>
             <p>Drivers: {event.numDrivers}</p>
             <p>Packers: {event.numPackers}</p>
-            <br/>
+            <br />
           </div>
         ))
       ) : (

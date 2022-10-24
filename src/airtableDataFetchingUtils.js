93191b5b15eb4c6840eb25078e511e63dd985bb4
Jason Cavanaugh <jascav418@gmail.com>
2022-10-24 08:45:57 -0400

    Added generic fetch call function to Airtable

--- src/airtableDataFetchingUtils.js
+++ src/airtableDataFetchingUtils.js
@@ -6,3 +6,16 @@ So this "base id" is the id for the Mott Haven Fridge/Grassroots Groceries Airta
 
 //"Base" here just has the normal definition
 export const AIRTABLE_URL_BASE = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}`;
+
+//Generic fetch call to Airtable
+const apiKey = import.meta.env.VITE_AIRTABLE_API_KEY;
+export const fetchAirtableData = async (url) => {
+  try {
+    const response = await fetch(url, {
+      headers: { Authorization: `Bearer ${apiKey}` },
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};

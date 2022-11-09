979735fd3619d5d5bb9b37ac288e19a8c32d2784
Jason Cavanaugh <jascav418@gmail.com>
2022-11-09 15:36:26 -0500

    View Events wip

--- src/pages/Events/ViewEvent.tsx
+++ src/pages/Events/ViewEvent.tsx
@@ -77,67 +77,144 @@ export function ViewEvent() {
 
   if (event === undefined) {
     console.error(
-      `Something went wrong. Event ${event} not found in futureEvents list`
+      `Something went wrong. Event ${event} not found in futureEvents list.`
     );
     return <div>Error...</div>;
   }
 
-  //RENDER LOGIC
-
-  //console.log("Logging scheduledSlotsForEvent", scheduledSlotsForEvent);
   scheduledSlots.records.sort((a, b) =>
     a.fields["First Name"] < b.fields["First Name"] ? -1 : 1
   );
+
   return (
-    <div>
-      <p>Day: {event.dateDisplay}</p>
-      <p>Time: {event.time}</p>
-      <p>Main Location: {event.mainLocation}</p>
-      <p>Total Participants: {event.numtotalParticipants}</p>
-      <p>Total # of Drivers: {event.numDrivers}</p>
-      <p>Total # of Packers: {event.numPackers}</p>
-      <p>Only Drivers: {event.numOnlyDrivers} </p>
-      <p>Only Packers: {event.numOnlyPackers} </p>
-      <p>Both Drivers & Packers: {event.numBothDriversAndPackers} </p>
-      <p># of Special Groups: {event.numSpecialGroups} </p>
+    <div className="px-14 py-16">
+      {/* Event Info */}
+      <h1 className=" text-3xl font-bold text-newLeafGreen">
+        {event.dateDisplay}
+      </h1>
+      <div className="h-4" />
+      <div className="flex gap-10">
+        <div className="flex flex-col ">
+          <p className="text-2xl">Time</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.time}
+          </p>
+        </div>
+        <div className="flex flex-col ">
+          <p className="text-2xl">Main Location</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.mainLocation}
+          </p>
+        </div>
+        <div className="flex flex-col ">
+          <p className="text-2xl"> Total Participants</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.numtotalParticipants}
+          </p>
+        </div>
+      </div>
+      <div className="h-12 " />
+
+      {/* Participant Breakdown */}
+      <div className="grid grid-cols-4 grid-rows-3 gap-3">
+        <h1 className="col-span-4 flex items-end rounded border text-3xl font-bold text-newLeafGreen">
+          Participant Breakdown
+        </h1>
+        <div className="flex flex-col rounded border">
+          <p className="text-2xl">Total # of Drivers</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.numDrivers}
+          </p>
+        </div>
+        <div className="flex flex-col rounded border">
+          <p className="text-2xl">Total # of Packers</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.numPackers}
+          </p>
+        </div>
+
+        <div className="col-span-2 flex flex-col rounded border md:col-span-1">
+          <p className="text-2xl">Both Drivers & Packers</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.numBothDriversAndPackers}
+          </p>
+        </div>
+
+        <div className="flex flex-col rounded border">
+          <p className="text-2xl">Only Drivers</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.numOnlyDrivers}
+          </p>
+        </div>
+        <div className="flex flex-col rounded border">
+          <p className="text-2xl">Only Packers</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.numOnlyPackers}
+          </p>
+        </div>
+        <div className="col-span-2 flex flex-col rounded border md:col-span-1">
+          <p className="text-2xl"># of Special Groups</p>
+          <p className="text-2xl font-semibold text-newLeafGreen">
+            {event.numSpecialGroups}
+          </p>
+        </div>
+
+        <div className="row-span-1 flex flex-col items-center justify-around rounded border md:row-span-2">
+          <button
+            className="rounded-full bg-pumpkinOrange px-5 py-3 font-extrabold text-white shadow-md shadow-newLeafGreen transition-all hover:-translate-y-1 hover:shadow-lg hover:shadow-newLeafGreen"
+            type="button"
+          >
+            View Special Groups
+          </button>
+          <button
+            className="rounded-full bg-pumpkinOrange px-5 py-3 font-extrabold text-white shadow-md shadow-newLeafGreen transition-all hover:-translate-y-1 hover:shadow-lg hover:shadow-newLeafGreen"
+            type="button"
+          >
+            + Add Special Group
+          </button>
+        </div>
+        <div className="flex justify-center rounded border"></div>
+      </div>
       <br />
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First</th>
-            <th>Last</th>
-            <th>Time</th>
-            <th>Participant Type</th>
-            <th>Confirmed</th>
-            <th>Special Group</th>
-            <th>Delivery Type</th>
-            <th>Contact</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scheduledSlots.records.map((scheduledSlot, idx) => {
-            return (
-              <tr key={scheduledSlot.id}>
-                <td>{idx + 1}</td>
-                <td>{scheduledSlot.fields["First Name"]}</td>
-                <td>{scheduledSlot.fields["Last Name"]}</td>
-                <td>
-                  {scheduledSlot.fields["Correct slot time"]["error"]
-                    ? "None"
-                    : scheduledSlot.fields["Correct slot time"]}
-                </td>
-                <td>{scheduledSlot.fields["Type"].length}</td>
-                <td>{scheduledSlot.fields["Confirmed?"] ? "Yes" : "No"}</td>
-                <td>{scheduledSlot.fields["Volunteer Status"]}</td>
-                <td>IDK</td>
-                <td>{scheduledSlot.fields["Email"]}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-        <tfoot></tfoot>
-      </table>
+      <div>
+        <table className="border-4 border-softGrayWhite">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First</th>
+              <th>Last</th>
+              <th>Time</th>
+              <th>Participant Type</th>
+              <th>Confirmed</th>
+              <th>Special Group</th>
+              <th>Delivery Type</th>
+              <th>Contact</th>
+            </tr>
+          </thead>
+          <tbody>
+            {scheduledSlots.records.map((scheduledSlot, idx) => {
+              return (
+                <tr key={scheduledSlot.id}>
+                  <td>{idx + 1}</td>
+                  <td>{scheduledSlot.fields["First Name"]}</td>
+                  <td>{scheduledSlot.fields["Last Name"]}</td>
+                  <td>
+                    {scheduledSlot.fields["Correct slot time"]["error"]
+                      ? "None"
+                      : scheduledSlot.fields["Correct slot time"]}
+                  </td>
+                  <td>{scheduledSlot.fields["Type"].length}</td>
+                  <td>{scheduledSlot.fields["Confirmed?"] ? "Yes" : "No"}</td>
+                  <td>{scheduledSlot.fields["Volunteer Status"]}</td>
+                  <td>IDK</td>
+                  <td>{scheduledSlot.fields["Email"]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+      </div>
     </div>
   );
 }

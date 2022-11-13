459777c4083f2fee874baae5a98e4e529ca75f5c
Jason Cavanaugh <jascav418@gmail.com>
2022-11-13 17:32:54 -0500

    Responsiveness on cards finished

--- src/pages/Events/EventCard.tsx
+++ src/pages/Events/EventCard.tsx
@@ -1,37 +1,8 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 //Assets
 import alert from "../../assets/alert.svg";
 import check from "../../assets/check.svg";
-
-import "./EventCard.css";
-
-//Custom hook to watch the dimensions of the screen
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
-
-function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return windowDimensions;
-}
 
 interface Props {
   eventId: string;
@@ -43,7 +14,7 @@ interface Props {
   packers: number;
 }
 
-const EventCardDesktop: React.FC<Props> = ({
+export const EventCard: React.FC<Props> = ({
   date,
   time,
   location,
@@ -52,176 +23,73 @@ const EventCardDesktop: React.FC<Props> = ({
   drivers,
   packers,
 }) => {
-  // return (
-  //   // <li className="rounded-xl border-4 border-softGrayWhite p-8">
-  //   <li className="event-card desktop">
-  //     {/* Date */}
-  //     <h2 className="header">{date}</h2>
-  //     <div style={{ height: "0.875rem" }} />
-  //     {/* Time & Main locations & Total participants & View Event Button */}
-  //     <div className="mid">
-  //       <div>
-  //         <p className="label">Time</p>
-  //         <h3 className="label bold-green">{time}</h3>
-  //       </div>
-  //       <div>
-  //         <p className="label">Main Location</p>
-  //         <h3 className="label bold-green line-clamp">{location}</h3>
-  //       </div>
-  //       <div>
-  //         <p className="label">Total Participants</p>
-  //         <h3 className="label bold-green">{participants}</h3>
-  //       </div>
-  //       <div className="view-event-btn-wrapper">
-  //         <Link to={`/events/${eventId}`}>
-  //           <button className="view-event-btn">View Event</button>
-  //         </Link>
-  //       </div>
-  //     </div>
-  //     <div style={{ height: "0.875rem" }} />
-  //     {/* Drivers & Packers */}
-  //     <div className="bottom">
-  //       <div className="wrapper">
-  //         <p className="label" style={{ border: "1px solid green" }}>
-  //           Drivers:
-  //         </p>
-  //         <h3 className="driver-fraction bold-green">
-  //           {drivers}/30{" "}
-  //           <img
-  //             style={{ padding: "0 0.125rem" }}
-  //             src={drivers >= 30 ? check : alert}
-  //             alt="wut"
-  //           />
-  //         </h3>
-  //       </div>
-  //       <div className="wrapper">
-  //         <p className="label">Packers:</p>
-  //         <h3 className="label bold-green">{packers}</h3>
-  //       </div>
-  //     </div>
-  //   </li>
-  // );
+  // Tailwind classes
+  const label = "text-sm md:text-base lg:text-xl";
+  const labelBold =
+    "text-sm font-semibold text-newLeafGreen md:text-base lg:text-xl";
 
   return (
-    <li className="event-card">
+    <li className="mb-4 rounded-xl border-2 border-softGrayWhite bg-softBeige px-4 py-3 lg:border-4 lg:px-8 lg:py-7">
       {/* Date */}
-      <h2 className="header">{date}</h2>
+      <h2 className="text-base font-bold text-newLeafGreen md:text-xl lg:text-2xl">
+        {date}
+      </h2>
       <div className="h-4"></div>
       {/* Time & Main locations & Total participants & View Event Button */}
-      <div className="details">
-        <div className="left">
-          <div className="col-span-1">
-            <p className="label">Time</p>
-            <h3 className="label bold-green">{time}</h3>
+      <div className="flex lg:gap-8">
+        <div className="grid grow auto-rows-min grid-cols-6 md:grid-cols-5 md:gap-1 lg:grow-0 lg:gap-y-2">
+          {/* Time */}
+          <div className="col-span-2 md:col-span-1">
+            <p className={label}>Time</p>
+            <h3 className={labelBold}>{time}</h3>
           </div>
-          <div className="col-span-3">
-            <p className="label">Main Location</p>
-            <h3 className="label bold-green line-clamp">{location}</h3>
+          {/* Main Location */}
+          <div className="dev-border col-span-4 md:col-span-4">
+            <p className={label}>Main Location</p>
+            <h3 className={`${labelBold} line-clamp-2`}>{location}</h3>
           </div>
-          <div className="flex flex-col gap-2 lg:flex-row lg:items-center">
-            <p className="label flex items-start">Drivers:</p>
-            <h3 className="driver-fraction label bold-green flex items-start gap-2 lg:gap-2">
+          {/* Drivers */}
+          <div className="col-span-2 grow-0 md:col-span-1">
+            <p className="event-card-label">Drivers:</p>
+          </div>
+          {/* Drivers Count */}
+          <div className="col-span-4 md:col-span-4 lg:col-span-2">
+            <h3 className="event-card-label-bold flex gap-4">
               {drivers}/30{" "}
               <img
-                style={{ padding: "0.35rem 0.125rem" }}
+                className="mt-1 w-4 md:w-6 lg:w-7"
                 src={drivers >= 30 ? check : alert}
                 alt="wut"
               />
             </h3>
           </div>
-          <div className="flex flex-col lg:flex-row lg:items-center lg:gap-2">
-            <p className="label">Packers:</p>
-            <h3 className="label bold-green">{packers}</h3>
+          {/* Packers */}
+          <div className="col-span-2 md:col-span-1">
+            <p className={label}>Packers:</p>
+          </div>
+          {/* Packers Count */}
+          <div className="col-span-4 md:col-span-4 lg:col-span-1">
+            <h3 className={labelBold}>{packers}</h3>
           </div>
         </div>
 
-        <div className="flex grow flex-col items-start border lg:flex-row lg:justify-between ">
+        {/* Total Participants & View Event Button */}
+        <div className="flex flex-col lg:grow lg:flex-row lg:items-start lg:justify-between ">
           <div>
-            <p className="label">Total Participants</p>
-            <h3 className="label bold-green">{participants}</h3>
+            <p className="text-sm md:text-base lg:text-xl">
+              Total Participants
+            </p>
+            <h3 className={labelBold}>{participants}</h3>
           </div>
-          <div className="view-event-btn-wrapper">
+          <div className="flex grow items-center lg:justify-end">
             <Link to={`/events/${eventId}`}>
-              <button className="view-event-btn">View Event</button>
+              <button className="rounded-full bg-newLeafGreen px-1 py-1 text-xs font-medium text-softGrayWhite shadow-md shadow-newLeafGreen transition-all hover:-translate-y-1 hover:shadow-lg hover:shadow-newLeafGreen md:px-4 md:py-2 lg:px-8 lg:py-4 lg:text-xl">
+                View Event
+              </button>
             </Link>
           </div>
         </div>
-        {/* Drivers & Packers */}
-        <div className="bottom"></div>
       </div>
     </li>
   );
-};
-
-const EventCardMobile: React.FC<Props> = ({
-  date,
-  time,
-  location,
-  participants,
-  eventId,
-  drivers,
-  packers,
-}) => {
-  return (
-    // <li className="rounded-xl border-4 border-softGrayWhite p-4">
-    <li className="event-card mobile">
-      {/* Date */}
-      <h2 className="header">{date}</h2>
-      <div style={{ height: "0.875rem" }} />
-      <div className="flex justify-between gap-2">
-        {/* Time & Drivers & Packers */}
-        <div className="flex flex-col gap-1">
-          <div className="grow">
-            <p className="label">Time</p>
-            <h3 className="label-bold-green">{time}</h3>
-          </div>
-          <h3 className="label">Drivers:</h3>
-          <h3 className="label">Packers:</h3>
-        </div>
-        {/* Location & Drivers count & Packers count */}
-        <div className="flex flex-col gap-1">
-          <div className="grow">
-            <p className="label">Main Location</p>
-            <h3 className="h-10 overflow-hidden text-ellipsis text-sm font-semibold text-newLeafGreen line-clamp-2">
-              {location}
-            </h3>
-          </div>
-          <h3 className="driver-fraction">
-            {drivers}/30{" "}
-            <img
-              style={{ padding: "0 0.125rem" }}
-              src={drivers >= 30 ? check : alert}
-              alt="wut"
-            />
-          </h3>
-          <h3 className="label bold-green">{packers}</h3>
-        </div>
-        {/* Total participants & Total participants count & View event */}
-        <div className="flex flex-col">
-          <div className="grow">
-            <p className="label">Total Participants</p>
-            <h3 className="label bold-green">{participants}</h3>
-          </div>
-          <Link to={`/events/${eventId}`}>
-            <button className="rounded-full bg-newLeafGreen px-3 py-2 text-xs font-semibold text-softGrayWhite shadow-md shadow-newLeafGreen transition-all hover:-translate-y-1 hover:shadow-lg hover:shadow-newLeafGreen">
-              View Event
-            </button>
-          </Link>
-          <div className="h-2" />
-        </div>
-      </div>
-    </li>
-  );
-};
-
-//Because the mobile card is kinda different from the desktop card in its layout, I've created two separate components for each.
-export const EventCard: React.FC<Props> = (props) => {
-  const { width } = useWindowDimensions();
-
-  //I hate this magic number. It corrosponds to the `sm` breakpoint in Tailwind, which is 640 pixels
-  // if (width <= 640) {
-  //   return <EventCardMobile {...props} />;
-  // } else {
-  return <EventCardDesktop {...props} />;
-  // }
 };

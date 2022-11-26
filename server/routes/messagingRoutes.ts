69f3439662218b87e2ebfa25250054a21529702b
Jason Cavanaugh <jascav418@gmail.com>
2022-11-26 03:37:02 -0500

    Fetch to Make working

--- server/routes/messagingRoutes.ts
+++ server/routes/messagingRoutes.ts
@@ -2,6 +2,11 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 //import fetch from "node-fetch";
+// ✅ Do this if using TYPESCRIPT
+import { RequestInfo, RequestInit } from "node-fetch";
+
+const fetch = (url: RequestInfo, init?: RequestInit) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(url, init));
 
 import { Request, Response } from "express";
 
@@ -14,6 +19,7 @@ const router = express.Router();
  */
 router.route("/api/messaging").get(
   asyncHandler(async (req: Request, res: Response) => {
+    console.log(process.env.MAKE_API_KEY);
     try {
       const resp = await fetch(
         "https://us1.make.com/api/v2/scenarios?teamId=1989&pg%5Blimit%5D=100",
@@ -24,7 +30,7 @@ router.route("/api/messaging").get(
           },
         }
       );
-      const data = resp.json();
+      const data = await resp.json();
       res.status(200).json(data);
     } catch (error) {
       console.error(error);

a5f94fd100854d3f726d5252ef4c7afc43a3446e
Jason Cavanaugh <jascav418@gmail.com>
2022-11-27 15:25:47 -0500

    Fetching coordinator and volunteer text messages from Make completed. Code still needs to be cleaned though

--- client/src/pages/Events/Messaging.tsx
+++ client/src/pages/Events/Messaging.tsx
@@ -1,4 +1,6 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { Loading } from "../../components/Loading";
+import { API_BASE_URL } from "../../httpUtils";
 
 const defaultText = "Jason this is some awesome text that I am typing rn";
 // Tailwind classes
@@ -12,51 +14,78 @@ const textArea =
 const btn =
   "rounded-full bg-pumpkinOrange px-3 py-2 text-sm font-semibold text-white shadow-md shadow-newLeafGreen transition-all hover:-translate-y-1 hover:shadow-lg hover:shadow-newLeafGreen lg:px-5 lg:py-3 lg:text-base lg:font-bold";
 
-const makeKey = import.meta.env.VITE_MAKE_API_KEY;
 export function Messaging() {
-  const [loading, setLoading] = useState(false);
-  const fetchData = async () => {
-    console.log("key", makeKey);
-    setLoading(true);
-    try {
-      const resp = await fetch(
-        "https://us1.make.com/api/v2/scenarios?teamId=1989&pg%5Blimit%5D=100",
-        {
-          headers: {
-            method: "GET",
-            Authorization: `Token ${makeKey}`,
-          },
-        }
-      );
-      const data = await resp.json();
-      console.log("data", data);
-    } catch (error) {
-      console.error(error);
-    }
-    setLoading(false);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const {
+    data: volunteerRecruitmentTextData,
+    status: volunteerRecruitmentTextStatus,
+    error: volunteerRecruitmentTextError,
+  } = useQuery(["fetchVolunteerRecruitmentTextBlueprint"], async () => {
+    const resp = await fetch(
+      `${API_BASE_URL}/api/messaging/volunteer-recruitment-text`
+    );
+    return resp.json();
+  });
+  const {
+    data: coordinatorRecruitmentTextData,
+    status: coordinatorRecruitmentTextStatus,
+    error: coordinatorRecruitmentTextError,
+  } = useQuery(["fetchCoordinatorRecruitmentTextBlueprint"], async () => {
+    const resp = await fetch(
+      `${API_BASE_URL}/api/messaging/coordinator-recruitment-text`
+    );
+    return resp.json();
+  });
 
-  if (loading) {
-    return <div>Loading</div>;
+  const volunteerTextLoading =
+    volunteerRecruitmentTextStatus === "loading" ||
+    volunteerRecruitmentTextStatus === "idle";
+
+  const coordinatorTextLoading =
+    coordinatorRecruitmentTextStatus === "loading" ||
+    coordinatorRecruitmentTextStatus === "idle";
+
+  if (!coordinatorTextLoading) {
+    console.log(coordinatorRecruitmentTextData);
   }
 
   return (
     <div className="flex h-1/3 flex-col gap-4">
       <h1 className={sectionHeader}>Recruitment</h1>
       <div className="flex grow flex-wrap gap-16 lg:px-32">
-        {/* Card */}
+        {/* Coordinators Recruitment card */}
         <div className={recruitCard}>
           <h2 className={cardHeader}>For Coordinators</h2>
-          <textarea className={textArea} defaultValue={defaultText} />
+          {coordinatorTextLoading ? (
+            <div className="relative w-full grow rounded-md border-4 border-softGrayWhite py-2 px-4">
+              <Loading size="medium" thickness="thicc" />
+            </div>
+          ) : (
+            <textarea
+              className={textArea}
+              defaultValue={
+                coordinatorRecruitmentTextData.response.blueprint.flow[4].mapper
+                  .body
+              }
+            />
+          )}
           <button className={btn}>Recruit Coordinators</button>
         </div>
-        {/* Card */}
+        {/* Participants RecruitmentCard */}
         <div className={recruitCard}>
           <h2 className={cardHeader}>For Participants</h2>
-          <textarea className={textArea} defaultValue={defaultText} />
+          {volunteerTextLoading ? (
+            <div className="relative w-full grow rounded-md border-4 border-softGrayWhite py-2 px-4">
+              <Loading size="medium" thickness="thicc" />
+            </div>
+          ) : (
+            <textarea
+              className={textArea}
+              defaultValue={
+                volunteerRecruitmentTextData.response.blueprint.flow[2].mapper
+                  .body
+              }
+            />
+          )}
           <button className={btn}>Recruit Participants</button>
         </div>
       </div>
--- server/routes/messaging.ts
+++ server/routes/messaging.ts
@@ -6,15 +6,41 @@ import { Request, Response } from "express";
 const router = express.Router();
 
 /**
- * @description Get all upcoming messages.
- * @route  GET /api/messaging/
+ * @description Get Tuesday recruitment text message blueprint from Make
+ * @route  GET /api/messaging/tuesday-recruitment-text
  * @access
  */
-router.route("/api/messaging").get(
+router.route("/api/messaging/volunteer-recruitment-text").get(
   asyncHandler(async (req: Request, res: Response) => {
     try {
       const resp = await fetch(
-        "https://us1.make.com/api/v2/scenarios?teamId=1989&pg%5Blimit%5D=100",
+        "https://us1.make.com/api/v2/scenarios/299639/blueprint",
+        {
+          headers: {
+            method: "GET",
+            Authorization: `Token ${process.env.MAKE_API_KEY}`,
+          },
+        }
+      );
+      const data = await resp.json();
+      res.status(200).json(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  })
+);
+
+/**
+ * @description Get Tuesday recruitment text message blueprint from Make
+ * @route  GET /api/messaging/tuesday-recruitment-text
+ * @access
+ */
+router.route("/api/messaging/coordinator-recruitment-text").get(
+  asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const resp = await fetch(
+        "https://us1.make.com/api/v2/scenarios/278585/blueprint",
         {
           headers: {
             method: "GET",

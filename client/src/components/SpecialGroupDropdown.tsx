36c45ebecdf7946650544ff08f04991c883b3a94
sophtsai <sophietsai31@gmail.com>
2022-11-30 20:25:45 -0500

    Front-end add alert/check messages

--- client/src/components/SpecialGroupDropdown.tsx
+++ client/src/components/SpecialGroupDropdown.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import plus from "../assets/plus.svg";
 import x from "../assets/greenX.svg";
-
-var specialGroupInput = document.getElementById('specialGroupInput') as HTMLInputElement | null;
+import alert from "../assets/alert.svg";
+import check from "../assets/check.svg";
 
 interface testList {
   
@@ -24,6 +24,8 @@ export const Dropdown = () => {
     })
   
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    // result filtering based on input
     const results = posts.filter(post => {
     if (e.target.value === "") return posts
     return post.title.toLowerCase().includes(e.target.value.toLowerCase())
@@ -54,6 +56,7 @@ export const Dropdown = () => {
   },
   // more data here
   ]
+
   return (
 
     <div className="px-4 py-2">
@@ -65,12 +68,18 @@ export const Dropdown = () => {
         type="text" id="specialGroupInput" placeholder="Search through groups..." value={state.query} onChange={handleChange}></input>
         </form>
         <button>
-            <img className="relative bottom-0.25 right-6 w-3 sm:w-4" src={x} alt="x" />
+            <img 
+              className="relative bottom-0.25 right-6 w-3 sm:w-4" 
+              src={x} 
+              alt="x" 
+            />
         </button>
       </div>
       
+      {/* Special Group Listing Dropdown */} 
       <ul className="hidden w-72 border-2 border-t-0 border-softGrayWhite rounded-lg text-newLeafGreen py-1"
       id="specialGroupDropdown">
+
         <div className="text-sm text-[#0E7575] px-2">
           Select existing group or create new one
         </div>
@@ -78,10 +87,10 @@ export const Dropdown = () => {
         {(state.query === '' ? "" : state.list.map(post => {
           return <li className="flex flex-row rounded-lg hover:cursor-pointer hover:bg-softGrayWhite px-2" key={post.title}>
             <img
-            className="w-2 mr-1 md:w-4"
-            src={ plus }
-            alt="plus-icon"
-          /> 
+              className="w-2 mr-1 md:w-4"
+              src={ plus }
+              alt="plus-icon"
+            /> 
           {post.title}
           </li>
         }))}
@@ -92,8 +101,34 @@ export const Dropdown = () => {
             {state.query} 
           </div>
         </li>
-        
+
       </ul> 
+
+      {/* Aleady registered message */}
+      <div className="flex flex-row items-center">
+        <img
+          className="mt-1 w-4 md:w-6 lg:w-7"
+          src={alert}
+          alt="alert-icon"
+        /> 
+        <div className="flex flex-col items-center leading-5 px-2 font-semibold text-newLeafGreen">
+          <div> This group is already registered for </div> 
+          <div> the event! </div>
+        </div>
+      </div>
+
+      {/* Ready message */}
+      <div className="flex flex-row items-center">
+        <img
+          className="mt-1 w-4 md:w-6 lg:w-7"
+          src={check}
+          alt="check-icon"
+        /> 
+        <div className="flex flex-col items-center leading-5 px-4 font-semibold text-newLeafGreen">
+          Ready to generate link!
+        </div>
+      </div>
     </div>
+    
   );
 };

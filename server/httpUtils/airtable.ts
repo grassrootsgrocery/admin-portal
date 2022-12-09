fa42b28f3721d39e2104b00dd4b6b332616b4a34
Jason Cavanaugh <jascav418@gmail.com>
2022-12-09 00:56:27 -0500

    Set server to point to production Airtable base

--- server/httpUtils/airtable.ts
+++ server/httpUtils/airtable.ts
@@ -5,7 +5,7 @@ So this "base id" is the id for the Mott Haven Fridge/Grassroots Groceries Airta
 const AIRTABLE_BASE_ID_PROD = "app7zige4DRGqIaL2";
 const AIRTABLE_BASE_ID_DEV = "app18BBTcWqsoNjb2";
 //"Base" here just has the normal definition
-export const AIRTABLE_URL_BASE = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID_DEV}`;
+export const AIRTABLE_URL_BASE = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID_PROD}`;
 
 /* Because I'm not sure that we want to display error messages from Airtable on the client, this is the 
 default error message that we will use when a request to Airtable fails

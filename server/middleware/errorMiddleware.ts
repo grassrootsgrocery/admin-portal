a8177e571cee63a0fcdbe4d631b25d438202ec70
zuechai <zuechai@gmail.com>
2023-03-16 16:59:52 -0400

    Add logger to errorMiddleware

--- server/middleware/errorMiddleware.ts
+++ server/middleware/errorMiddleware.ts
@@ -1,11 +1,16 @@
 import type { ErrorRequestHandler } from "express";
 import { INTERNAL_SERVER_ERROR } from "../httpUtils/statusCodes";
+import { logger } from "../loggerUtils/logger";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const stack = process.env.NODE_ENV === "production" ? null : err.stack;
   const statusCode = err.status || INTERNAL_SERVER_ERROR;
+
+  logger.error(`${statusCode} ${err.message} ${stack}`);
+
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    stack,
   });
 };

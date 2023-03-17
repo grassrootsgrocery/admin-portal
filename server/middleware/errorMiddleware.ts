98d35944c02a9deb09a744ab30f4365ac2571b30
zuechai <zuechai@gmail.com>
2023-03-17 10:12:31 -0400

    Use nullish operator to print an empty string

--- server/middleware/errorMiddleware.ts
+++ server/middleware/errorMiddleware.ts
@@ -4,7 +4,7 @@ import { logger } from "../loggerUtils/logger";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   const statusCode = err.status || INTERNAL_SERVER_ERROR;
-  logger.error(`${statusCode} ${err.message} ${err.stack}`);
+  logger.error(`${statusCode} ${err.message} ${err.stack ?? ""}`);
 
   res.status(statusCode);
   res.json({

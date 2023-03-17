c00cfcf68a246945a28c76dcd2b1cc9afb0a2c8a
zuechai <zuechai@gmail.com>
2023-03-17 10:04:30 -0400

    Revert the stack to prior state so the server doesn't print null

--- server/middleware/errorMiddleware.ts
+++ server/middleware/errorMiddleware.ts
@@ -3,14 +3,12 @@ import { INTERNAL_SERVER_ERROR } from "../httpUtils/statusCodes";
 import { logger } from "../loggerUtils/logger";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  const stack = process.env.NODE_ENV === "production" ? null : err.stack;
   const statusCode = err.status || INTERNAL_SERVER_ERROR;
-
-  logger.error(`${statusCode} ${err.message} ${stack}`);
+  logger.error(`${statusCode} ${err.message} ${err.stack}`);
 
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };

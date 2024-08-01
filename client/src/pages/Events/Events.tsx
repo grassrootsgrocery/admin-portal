799fba5646a3409b559f33aa486abcc37bbd6d01
Kenji Kono <kenjikono@Kenjis-MBP.localdomain>
2024-08-01 14:40:45 -0400

    added search bar in main events page

--- client/src/pages/Events/Events.tsx
+++ client/src/pages/Events/Events.tsx
@@ -5,6 +5,8 @@ import { useAuth } from "../../contexts/AuthContext";
 import { Navigate } from "react-router-dom";
 import { useFutureEvents } from "../eventHooks";
 import { toastNotify } from "../../utils/ui";
+import { SearchButton } from "../../components/SearchButton";
+import { useState } from "react";
 
 const newEventLink =
   "https://airtable.com/shrETAYONKTJMVTnZ?prefill_Supplier=Rap+4+Bronx&prefill_Start+Time=01/01/2023+09:00am&prefill_End+Time=01/01/2023+01:00pm&prefill_First+Driving+Slot+Start+Time=01/01/2023+10:30am&prefill_How+long+should+each+Driver+Time+Slot+be?=0:15&prefill_Max+Count+of+Drivers+Per+Slot=30&prefill_How+long+should+the+Logistics+slot+be?=1:30&prefill_Maximum+number+of+drivers+needed+for+this+event+(usually+30)?=30&prefill_Max+Count+of+Distributors+Per+Slot=30";
@@ -39,6 +41,9 @@ export function Events() {
     console.error(error);
     return <div>Error...</div>;
   }
+  
+
+  const [searchValue, setSearchValue] = useState("");
 
   return (
     <>
@@ -49,6 +54,7 @@ export function Events() {
           <h1 className="text-2xl font-bold text-newLeafGreen md:text-4xl">
             Upcoming Events
           </h1>
+          <SearchButton value={searchValue} onChange={setSearchValue} />
           <a href={newEventLink} target="_blank" rel="noopener noreferrer">
             <button
               className="h-10 w-10 rounded-full bg-pumpkinOrange text-3xl font-semibold text-white sm:h-12 sm:w-12 lg:h-auto lg:w-auto lg:py-3 lg:px-5 lg:text-xl lg:shadow-md lg:shadow-newLeafGreen lg:transition-all lg:after:content-['_Add_Saturday_Event'] lg:hover:-translate-y-1 lg:hover:shadow-lg lg:hover:shadow-newLeafGreen"
